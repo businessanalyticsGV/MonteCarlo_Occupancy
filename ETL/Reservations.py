0a678757e06fc3b38fb4d63e0ca1bf7fa6c469f2
Alexis Saul Alvarez Soto <alexisalvarez@grupovidanta.com>
2019-04-17 19:02:43 -0500

    Added python extraction file in case the server's not available

--- ETL/Reservations.py
+++ ETL/Reservations.py
@@ -1,3 +1,28 @@
+import time
+start = time.time()
+#############################################################################
+#############################################################################
+########### EXTRACTION IN CASE THE SERVER'S NOT AVAILABLE ###################
+#############################################################################
+#############################################################################
+
+### I.- EXTRACTION
 import pandas as pd
 import numpy as np
+
+path = 'C:/Users/alexisalvarez/OneDrive - Grupo Vidanta/UPDATE/Work/00. QVDs/Extraction/DBs/'
+
+ls_cols = ['FirstNight','Area','SiteGroup','Roomtype_Orig','Semana_Myn']
+ls_targ = '%ReservationNumber'
+ls_cols = ls_cols + [ls_targ]
+df = pd.read_csv(path+'Reservations.csv')
+df = df[(df['Año']>=2015) & (df['ReservationStatus']!='Canceled') & (df['Deposits']=='No') & (df['NoShow']=='No')]
+df = df.groupby([c for c in ls_cols if c != ls_targ], as_index = False)[[ls_targ]].count()
+df.to_csv('training.csv', index = False)
+
+print(time.time()-start)
+
+
+
+
 
---
+++ Modelling/Deseasonalize.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('Z:/Modelos BA/MonteCarlo_Occupancy/ETL/Reservations_Work.csv')
+print(df.shape)
+print(df.head())
+
