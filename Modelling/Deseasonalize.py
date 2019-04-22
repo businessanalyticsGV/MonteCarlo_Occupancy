d382ea5432a5ea0a31146cef9c0c390abe2f9a13
Alexis Saul Alvarez Soto <alexisalvarez@grupovidanta.com>
2019-04-21 20:23:40 -0500

    pull

--- Modelling/Deseasonalize.py
+++ Modelling/Deseasonalize.py
@@ -6,18 +6,37 @@
 import pandas as pd
 import numpy as np
 import os
+pd.set_option('display.max_columns',500)
 
 os.chdir('../ETL')
 file = [f for f in os.listdir() if f[-4:] == '.csv'][0]
 df = pd.read_csv(file)
+df['FirstNight'] = pd.to_datetime(df['FirstNight'])
 
 ##################################################
 ############## PIVOT #############################
 ##################################################
+# temp = 'IN'
 df = df[(df['Area'] == 'Nuevo Vallarta') & (df['SiteGroup'] == 'The Grand Mayan') & \
-        (df['Roomtype_Orig'] == 'Grand Mayan Master Room')]
+        (df['Roomtype_Orig'] == 'Grand Mayan Master Room') & (df['Mix_NA_IN'] == 'IN') &\
+        (df['FirstNight']>='01/01/2017') & (df['FirstNight']<='31/12/2017')]
 
-df['FirstNight'] = pd.to_datetime(df['FirstNight'])
+# temp = ['Season Int' if temp == 'IN' else 'Temp Nal' for temp in list(df['Mix_NA_IN'])[0]]
+# print(temp)
+#################################################
+##################################################
+##################################################
+
+df = df[['FirstNight','Nights','Semana_Myn','Season Int','Reservations']]
+
+#### I.- Savg(t)
+
+# 
+df_card = df.groupby(['Season Int'], as_index = False)[['FirstNight']].count()
+df_card.rename(columns ={'FirstNight':'Card'}, inplace =True)
+print(df_card)
+exit()
+df = df.merge(df_card,how='left', on = ['Season Int'])
 
 print(df.shape)
 print(df.head())
@@ -25,11 +44,7 @@ print(min(df['FirstNight']))
 print(max(df['FirstNight']))
 
 
-# ls_toIter = ['Area','SiteGroup','Roomtype_Orig']
 
-# ls = [(it,list(np.unique(df[it]))) for it in ls_toIter]
-# for i in ls:
-#     print(i)
 
 
 
